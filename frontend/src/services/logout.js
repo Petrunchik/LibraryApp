eaa6f5cdc949c52ff852d467ead309f5ea9ab113
Petr <petrlinkov1376@gmail.com>
2026-06-18 12:01:43 +0300

    Исправлена ошибка связанная с выходом из профиля

--- frontend/src/services/logout.js
+++ frontend/src/services/logout.js
@@ -1,7 +1,14 @@
 import { apiClient } from "./ApiClient"
+import { RedirectToHome } from "./redirectToHome"
 
 export const logout = async () => {
-    return apiClient.logoutUser()
+    const result = await apiClient.logoutUser()
+
+    if (result.success) {
+        RedirectToHome()
+    }
+
+    return result
 }
 
 export const logoutUser = logout
